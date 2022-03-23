9293ab274a7ba25e93dc9eb9646d56cff41922c7
ChengyuanSha <15cs69@queensu.ca>
2022-03-22 22:14:09 -0400

    finish basuic UI

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react"
-
+import AddTask from "./components/AddTask";
 
 
 function App() {
 	// global states, can be used in other components 
+	const [showAddTask, setShowAddTask] = useState(false)
 	const [tasks, setTasks] = useState([
 		{
 			id: 1,
@@ -39,11 +40,20 @@ function App() {
 		setTasks(tasks.map((task) => task.id === id ? {...task, reminder:!task.reminder} : task))
 		// console.log(id)
 	}
+ 
+	// add task
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1
+		const newTask = {id, ...task}
+		setTasks([...tasks, newTask])
+		// console.log(task)
+	}
 
 
   return (
     <div className="container">
-      <Header/>
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+			{showAddTask && <AddTask onAdd={addTask} />}
 			{tasks.length > 0 ? 
 			(<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) 
 			: ('No task to show')}      
